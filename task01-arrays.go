243cb853896d5e19a265abed249548e9c9beb386
zhastlek <zhastlek95@gmail.com>
2022-05-06 00:36:49 +0600

    homework

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,9 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
-	return
+	for _, value := range input {
+		result += value
+	}
+	result = result / float32(len(input))
+	return result
 }
--- task02-slice.go
+++ task02-slice.go
@@ -2,5 +2,9 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	return
+	result = make([]int64, len(input))
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, input[i])
+	}
+	return result
 }
